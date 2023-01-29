be50dec54c24ec018ebc01b20600b06aff7ea986
William <williamgao904@gmail.com>
2023-01-29 17:47:25 -0600

    hi christos

---
+++ my-app/App.js
@@ -0,0 +1,109 @@
+import React from "react";
+import {
+  Text,
+  Link,
+  HStack,
+  Center,
+  Heading,
+  Switch,
+  useColorMode,
+  NativeBaseProvider,
+  extendTheme,
+  VStack,
+  Box,
+  StatusBar,
+  IconButton,
+  Icon
+} from "native-base";
+
+
+// Define the config
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+};
+
+// extend the theme
+export const theme = extendTheme({ config });
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <HeaderThing />
+        <VStack space={4} alignItems="left">
+          <Stack />
+        </VStack>
+    </NativeBaseProvider>
+  );
+}
+
+// Color Switch Component
+function ToggleDarkMode() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <HStack space={2} alignItems="center">
+      <Text>Dark</Text>
+      <Switch
+        isChecked={colorMode === "light"}
+        onToggle={toggleColorMode}
+        aria-label={
+          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
+        }
+      />
+      <Text>Light</Text>
+    </HStack>
+  );
+}
+
+function Stack() {
+  return <VStack space={4} alignItems="right">
+      <Center w="64" h="20" bg="indigo.300" rounded="md" shadow={3} />
+      <Center w="64" h="20" bg="indigo.500" rounded="md" shadow={3} />
+      <Center w="64" h="20" bg="indigo.700" rounded="md" shadow={3} />
+    </VStack>;
+}
+
+function Menu() {
+  return <Box w="90%" alignItems="center">
+      <Menu w="190" trigger={triggerProps => {
+      return <Pressable accessibilityLabel="More options menu" {...triggerProps}>
+              <HamburgerIcon />
+            </Pressable>;
+    }}>
+        <Menu.Item>Arial</Menu.Item>
+        <Menu.Item>Nunito Sans</Menu.Item>
+        <Menu.Item>Roboto</Menu.Item>
+        <Menu.Item>Poppins</Menu.Item>
+        <Menu.Item>SF Pro</Menu.Item>
+        <Menu.Item>Helvetica</Menu.Item>
+        <Menu.Item isDisabled>Sofia</Menu.Item>
+        <Menu.Item>Cookie</Menu.Item>
+      </Menu>
+    </Box>;
+}
+
+function AppBar() {
+  return <>
+      <StatusBar bg="#3700B3" barStyle="light-content" />
+      <Box safeAreaTop bg="violet.600" />
+      <HStack bg="violet.800" px="1" py="3" justifyContent="space-between" alignItems="center" w="100%" maxW="350">
+        <HStack alignItems="center">
+          <IconButton icon={<Icon size="sm" as={MaterialIcons} name="menu" color="white" />} />
+          <Text color="white" fontSize="20" fontWeight="bold">
+            Home
+          </Text>
+        </HStack>
+        <HStack>
+          <IconButton icon={<Icon as={MaterialIcons} name="favorite" size="sm" color="white" />} />
+          <IconButton icon={<Icon as={MaterialIcons} name="search" size="sm" color="white" />} />
+          <IconButton icon={<Icon as={MaterialIcons} name="more-vert" size="sm" color="white" />} />
+        </HStack>
+      </HStack>
+    </>;
+}
+
+function HeaderThing() {
+  return <Center>
+      <AppBar />
+    </Center>;
+}
